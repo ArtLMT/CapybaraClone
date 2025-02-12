2a30c9fc22ab5a482b4dcfca006346d36f1ef236
le minh thanhe <leminhthanh280425@gmail.com>
2025-02-12 14:51:58 +0700

    thay đổi tham số ở các hàm vì dư thừa

--- assets/constructor.js
+++ assets/constructor.js
@@ -8,45 +8,45 @@ export function Character( name, hp, atk, def , counterRate) {
   }
 
 // sử dụng Object Prototype để tiết kiệm bộ nhớ:
-Character.prototype.damageCalculator = function (user, target) { 
-        let damage = user.atk - target.def > 0 ? user.atk - target.def : 0 // Dùng toán tử 3 ngôi để kiểm tra
-      // let damage = Math.max (user.atk - target.def, 0) // Dùng hàm max để tìm giá trị lớn nhất giữa các tham số truyền vào
+Character.prototype.damageCalculator = function (target) { 
+        let damage = this.atk - target.def > 0 ? this.atk - target.def : 0 // Dùng toán tử 3 ngôi để kiểm tra
+      // let damage = Math.max (this.atk - target.def, 0) // Dùng hàm max để tìm giá trị lớn nhất giữa các tham số truyền vào
       return damage
 }
 
-Character.prototype.attack = function (user, target) {
-    target.hp -= this.damageCalculator(user, target)
-    console.log(`${user.name} đấm vỡ alo ${target.name}, ${target.name} mất ${this.damageCalculator(user,target)}, còn lại ${target.hp} máu`)
+Character.prototype.attack = function (target) {
+    target.hp -= this.damageCalculator(target)
+    console.log(`${this.name} đấm vỡ alo ${target.name}, ${target.name} mất ${this.damageCalculator(target)}, còn lại ${target.hp} máu`)
     // thêm tỉ lệ phản công, chỉ hoạt đống nếu còn sống
     // đi đời rồi thì phản công kiểu gì?
     if ((Math.random() >= (1 - target.counterRate)) && this.isAlive(target)) {
-        user.hp -= this.damageCalculator(target,user)
-        console.log(`${target.name} phản công, gây ${target.atk} sát thương lên ${user.name}, ${user.name} còn ${user.hp} máu`)
+        this.hp -= this.damageCalculator(target)
+        console.log(`${target.name} phản công, gây ${target.atk} sát thương lên ${this.name}, ${this.name} còn ${this.hp} máu`)
     }
 }
 
 Character.prototype.isAlive = function (user) {
-  return user.hp > 0
+  return this.hp > 0
 }
 
 //   // object sử lý các logic khi đánh nhau
 // const battle = {
 //   damageCalculator(user, target) {
-//       let damage = user.atk - target.def > 0 ? user.atk - target.def : 0 // Dùng toán tử 3 ngôi để kiểm tra
-//       // let damage = Math.max (user.atk - target.def, 0) // Dùng hàm max để tìm giá trị lớn nhất giữa các tham số truyền vào
+//       let damage = this.atk - target.def > 0 ? this.atk - target.def : 0 // Dùng toán tử 3 ngôi để kiểm tra
+//       // let damage = Math.max (this.atk - target.def, 0) // Dùng hàm max để tìm giá trị lớn nhất giữa các tham số truyền vào
 //       return damage
 //   },
 //   attack(user, target) {
 //       target.hp -= this.damageCalculator(user, target)
-//       console.log(`${user.name} đấm vỡ alo ${target.name}, ${target.name} mất ${this.damageCalculator(user,target)}, còn lại ${target.hp} máu`)
+//       console.log(`${this.name} đấm vỡ alo ${target.name}, ${target.name} mất ${this.damageCalculator(user,target)}, còn lại ${target.hp} máu`)
 //       // thêm tỉ lệ phản công, chỉ hoạt đống nếu còn sống
 //       // đi đời rồi thì phản công kiểu gì?
 //       if ((Math.random() >= (1 - target.counterRate)) && this.isAlive(target)) {
-//           user.hp -= this.damageCalculator(target,user)
-//           console.log(`${target.name} phản công, gây ${target.atk} sát thương lên ${user.name}, ${user.name} còn ${user.hp} máu`)
+//           this.hp -= this.damageCalculator(target,user)
+//           console.log(`${target.name} phản công, gây ${target.atk} sát thương lên ${this.name}, ${this.name} còn ${this.hp} máu`)
 //       }
 //   },
 //   isAlive(user) {
-//       return user.hp > 0
+//       return this.hp > 0
 //   }
 // }
